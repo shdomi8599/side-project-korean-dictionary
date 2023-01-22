a651725f5a63ce0ee0baf2ac292b989239c87b51
shdomi8599 <shdomi@naver.com>
2023-01-23 04:44:15 +0900

    챔프 피드백 페이지 디테일 업그레이드

--- src/components/FeedbackText.js
+++ src/components/FeedbackText.js
@@ -3,23 +3,12 @@ import BlockDisplay from "./BlockDisplay"
 
 const FeedbackText = ({feedbackTextInput,feedbackText,textTogle,textTogleHandler,id}) =>{
 
-const textValue=useRef();   
-
 let spanStyle = 'fake_placeholder'
-
-
-
-if(feedbackText !== '피드백을 적어주세요!'){
-    spanStyle = 'fake_placeholder champFeedText' ;
-}
-
 let divStyle = "feedback_text"
 if(localStorage.getItem(`feedText${id}`)&&textTogle){
     divStyle= "feedback_text champFeedText"
-    spanStyle = "feedback_text champFeedText"
+    spanStyle = "feedback_text"
 }
-
-
 
     return <div className={divStyle}>
         {textTogle? <span className={spanStyle} onClick={textTogleHandler}>
