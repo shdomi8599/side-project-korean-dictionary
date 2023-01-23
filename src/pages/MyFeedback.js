452bbb96fab8db56a10b56c604fdccd4dd59ead4
shdomi8599 <shdomi@naver.com>
2023-01-24 01:30:09 +0900

    네비바 고정, 나만의 피드백 높이 조절 반응형으로 개선

--- src/pages/MyFeedback.js
+++ src/pages/MyFeedback.js
@@ -2,10 +2,11 @@ import EmptyFeedback from "../components/EmptyFeedback"
 import MyFeedbackCard from "../components/MyFeedbackCard"
 
 const MyFeedback = ({ champData }) => {
-
+    let CheckHeight;
     let myChampArr;
     if(localStorage.getItem('myChampList') !==null){
         myChampArr = JSON.parse(localStorage.getItem('myChampList'))
+        CheckHeight = Math.ceil(myChampArr.length/6)
     }
    
     let myFeedbackStyle = "myfeedback"
@@ -14,13 +15,26 @@ const MyFeedback = ({ champData }) => {
         myFeedbackStyle = "myfeedback_flex"
     }
 
+    let height = 100
+
+    if(CheckHeight>2) {
+       height = height + (CheckHeight-2)*35
+    }
+
+    const backImgHeight = {height : `${height}vh`}
+
     return (
+        <>
         <div className="myfeedback_back">
             <div className={myFeedbackStyle}>
                 {localStorage.myChampList === '[]'||localStorage.getItem('myChampList') ===null? <EmptyFeedback /> :
                     myChampArr.map(champ => <MyFeedbackCard key={champ} id={champ} champData={champData} />)}
             </div>
         </div>
+        <div className="myfeedback_back_img" style={backImgHeight}>
+
+        </div>
+        </>
     )
 }
 
